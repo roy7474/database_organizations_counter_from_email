ae6a38434142e12f524299a740142ebe4690394d
Roy Contreras Benitez <roy7474@gmail.com>
2024-01-16 16:14:27 -0500

    updated file name, now the results from the table match the desired results from the sample data

--- database_organizations_email_counter.py
+++ database_organizations_email_counter.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-conn = sqlite3.connect('emaildb.sqlite')
+conn = sqlite3.connect('organizations_from_emails_in_mbox_file.sqlite')
 cur = conn.cursor()
 
 cur.execute('DROP TABLE IF EXISTS Counts')
@@ -9,7 +9,7 @@ cur.execute('''
 CREATE TABLE Counts (organization TEXT, count INTEGER)''')
 
 fname = input('Enter file name: ')
-if (len(fname) < 1): fname = 'mbox-short.txt'
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
     if not line.startswith('From: '): continue
@@ -29,7 +29,7 @@ for line in fh:
     conn.commit()
 
 # https://www.sqlite.org/lang_select.html
-sqlstr = 'SELECT organization, count FROM Counts ORDER BY count DESC LIMIT 10'
+sqlstr = 'SELECT organization, count FROM Counts ORDER BY count DESC'
 
 for row in cur.execute(sqlstr):
     print(str(row[0]), row[1])
